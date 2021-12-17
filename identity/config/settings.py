ff140bfc98a10d95c6384a67e30d89b392b88adf
Parsa <salehipour.edu@gmail.com>
2021-12-17 15:23:27 +0330

    reading secret keys from environment file

--- identity/config/settings.py
+++ identity/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n7f6jw=gvf1x7n2v$d4p+c1ldp48a2f%@amohc9(8r=k44&2ms'
+SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=1))
